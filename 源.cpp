f2f06b592ff44382a0b039819a942ea5a4a407cf
tong'xun hu <17321690973@163.com>
2023-12-11 14:03:56 +0800

    成功导入住户信息

--- 源.cpp
+++ 源.cpp
@@ -1,14 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct {           //定义住户信息结构体
 	char name[20];        //户主姓名
-	char gender;       //户主性别
-	int phonenumber; //联系电话
+	char gender[2];         //户主性别
+	char phonenumber[20]; //联系电话
 	int building;         //楼号
 	int unit;            //单元号
 	int room;         //房间号
 	float area;       //面积
-	float paymonth;    //物业月单价
+	int paymonth;    //物业月单价
 	float publicpay;  //公摊水电费
 }residentinfo;
 
@@ -17,7 +18,7 @@ int residentcount = 0;
 
 
 void menu();
-void importresidents();
+void importresidents(char path[]);
 void querymenu();
 void querybyroom();
 void querybyname();
@@ -47,7 +48,11 @@ void menu()
 		switch (choice)
 		{
 		case 1:
-			importresidents();
+			char path[100];
+			printf("请输入需要导入的文件路径:");
+			scanf("%s", path);
+
+			importresidents(path);
 		case 2:
 			querymenu();
 		case 3:
@@ -60,34 +65,30 @@ void menu()
 
 	}
 }
-void importresidents()
+void importresidents(char path[])
 {
-
 	FILE* fp;
-	errno_t err = fopen_s(&fp, "住户信息.txt", "r");
-	if (err == 1)
-	{
-		printf("file error\n");
-		exit(1);
-	}
+	fp=fopen( path, "r+");
+	if ( NULL == fp)
+		return ;
 	fscanf_s(fp, "%d", &residentcount);
-	printf("%d", residentcount);
 	for (int i = 0; i < residentcount; i++)
 	{
-		residentinfo resident;
-		fscanf_s(fp, "%s %c %d %d %d %d %lf %lf %lf", resident.name, &resident.gender,
-			resident.phonenumber, &resident.building, &resident.unit, &resident.room,
-			&resident.area, &resident.paymonth, &resident.publicpay);
-		residents[i] = resident;
-	}	
-		fclose(fp);
-	
+		fscanf_s(fp, "%s %s %s %d %d %d %f %d %f\n",residents[i].name, 20, residents[i].gender, 20, 
+			residents[i].phonenumber, 20, &residents[i].building,
+			&residents[i].unit, &residents[i].room, &residents[i].area,
+			&residents[i].paymonth, &residents[i].publicpay);
+	}
+	printf("导入成功，共有%d条信息\n", residentcount);
+	fclose(fp);
+	system("pause");
+	menu();
 }
 
 
 void querymenu()
 {
-	int choice;
+	 int choice;
 	printf("                            ………………欢迎来到缴费情况查询………………\n");
 	printf("                                        请选择：           \n");
 	printf("                             ------------------------------------------\n");
@@ -124,5 +125,5 @@ void payment() {
 }
 void overduecount()
 {
-
+	
 }
